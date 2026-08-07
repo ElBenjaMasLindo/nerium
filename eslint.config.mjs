32f55643a4d43717821d2a7890189f6c5ca92d43
ElBenjaMasLindo <Git@ElBenjaMasLindo.com>
2026-08-07 19:47:33 -0300

    chore: upgrade sadist to v0.6.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,9 +4,6 @@ import tsParser from "@typescript-eslint/parser";
 export default [
   ...strict,
   {
-    ignores: [".notes/**", "dist/**", "node_modules/**"],
-  },
-  {
     files: ["src/**/*.ts"],
     languageOptions: {
       parser: tsParser,
